16946a65e05a98a0393bfb013616d6ea821cd501
rahulpattanshetty <rahul@royalbrothers.com>
2020-04-27 00:11:53 +0530

    initial comment

---
+++ lib/vehicle_company.rb
@@ -0,0 +1,11 @@
+require "vehicle_company/version"
+
+module VehicleCompany
+  class Error < StandardError; end
+  
+  def self.all
+    ["Maruti","Suzuki"]
+  end
+  
+
+end
---
+++ lib/vehicle_company/version.rb
@@ -0,0 +1,3 @@
+module VehicleCompany
+  VERSION = "0.1.0"
+end
